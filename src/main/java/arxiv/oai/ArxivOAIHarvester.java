1c136b6345fafcf68cb3aa8f1ad3a9ced7fb6822
Hoang Long <litpuvn@gmail.com>
2016-10-25 17:06:55 +0700

    support resumption token handling

--- src/main/java/arxiv/oai/ArxivOAIHarvester.java
+++ src/main/java/arxiv/oai/ArxivOAIHarvester.java
@@ -6,8 +6,12 @@ import arxiv.xml.XMLParser;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
+import org.apache.commons.codec.net.URLCodec;
+import org.apache.http.client.utils.URLEncodedUtils;
 
 import javax.ws.rs.core.MediaType;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -47,26 +51,38 @@ public class ArxivOAIHarvester {
      * @throws BadArgumentException
      */
     public ParsedXmlResponse listRecords(Date fromDate, Date toDate) throws UniformInterfaceException, BadArgumentException {
-        if (fromDate == null) {
-            throw new BadArgumentException("Expect from Date not null");
-        }
+       return this.listRecords(fromDate, toDate, null);
+    }
+
+    public ParsedXmlResponse listRecords(Date fromDate, Date toDate, String resumptionToken) throws UniformInterfaceException, BadArgumentException {
 
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         WebResource service = this.webResource.path("oai2")
                 .queryParam("verb", "ListRecords")
-                .queryParam("from", df.format(fromDate))
-                .queryParam("metadataPrefix", METADATA_PREFIX_RAW)
         ;
 
-        log.info("Querying repository: " + service.getURI());
-
-        if (toDate != null) {
-            if (fromDate.after(toDate)) {
-                throw new BadArgumentException("fromDate must be less than or equal to untilDate");
+        if (resumptionToken != null && resumptionToken.length() > 0) {
+            log.info("Found resumption token: " + resumptionToken);
+            service = service.queryParam("resumptionToken", resumptionToken);
+        } else {
+            if (fromDate == null) {
+                throw new BadArgumentException("Expect from Date not null");
             }
 
-            service.queryParam("until", df.format(toDate));
+            service = service.queryParam("from", df.format(fromDate))
+                    .queryParam("metadataPrefix", METADATA_PREFIX_RAW)
+            ;
+
+            if (toDate != null) {
+                if (fromDate.after(toDate)) {
+                    throw new BadArgumentException("fromDate must be less than or equal to untilDate");
+                }
+
+                service = service.queryParam("until", df.format(toDate));
+            }
         }
+
+        log.info("Querying repository: " + service.getURI());
 
         return this.xmlParser.parse(this.getXmlDataFromArxiv(service));
     }
